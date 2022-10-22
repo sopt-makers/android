db9a19a69d62ce9e60841a19be5dbb697c205a83
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-22 15:31:07 +0900

    feat: Create NoticeDetailScreen
    feature/ui-notice

---
+++ app/src/main/java/org/sopt/official/feature/notice/NoticeDetailScreen.kt
@@ -0,0 +1,19 @@
+package org.sopt.official.feature.notice
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.official.style.SoptTheme
+
+@Composable
+fun NoticeDetailScreen() {
+    Box{}
+}
+
+@Preview
+@Composable
+fun PreviewNoticeDetailScreen() {
+    SoptTheme {
+        NoticeScreen()
+    }
+}
